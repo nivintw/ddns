484557a12842a8534d37729453ecbc112784319e
Tyler Nivin <tyler.nivin@gmail.com>
2024-08-29 23:17:20 -0500

    refactor: clean up args.py

--- src/ddns_digital_ocean/args.py
+++ src/ddns_digital_ocean/args.py
@@ -17,12 +17,57 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright 2023 - 2024, Tyler Nivin <tyler@nivin.tech> and the ddns-digital-ocean contributors
 
+from __future__ import annotations
+
 import argparse
 import textwrap
 
 from ddns_digital_ocean import api_key_helpers, info, ip, logs
 
 from . import domains
+
+
+def configure_domains_subparser(subparsers: argparse._SubParsersAction[argparse.ArgumentParser]):
+    """Configure the parser for the domains subparser"""
+    parser_domains = subparsers.add_parser(name="domains", help="View and configure domains.")
+    parser_domains.set_defaults(func=domains.main)
+
+    group_domains = parser_domains.add_mutually_exclusive_group(required=True)
+    group_domains.add_argument(
+        "-l",
+        "--list",
+        help="List domains registered in your DigitalOcean account, indicating which are managed.",
+        action="store_true",
+    )
+
+    group_domains.add_argument(
+        "-a",
+        "--add",
+        help="Add <domain> to domains managed by ddns-digital-ocean.",
+        metavar=("<domain>"),
+    )
+
+
+def configure_ip_lookup_subparser(subparsers: argparse._SubParsersAction[argparse.ArgumentParser]):
+    parser_ip_server = subparsers.add_parser(
+        name="ip_lookup_config",
+        help=("Update the service/server used to lookup your public IP address."),
+    )
+    parser_ip_server.set_defaults(func=ip.view_or_update_ip_server)
+    parser_ip_server.add_argument(
+        "--url",
+        help=(
+            "The URL of the server to use for obtaining your current IP address. "
+            "NOTE: Expects the servers response to a GET request to have a .text response. "
+            "Example: https://api.ipify.org"
+        ),
+    )
+    parser_ip_server.add_argument(
+        "--ip-mode",
+        choices=["4", "6"],
+        default="4",
+        help=("IPv4 or IPv6. Which IP address to update. Default: %(default)s"),
+    )
 
 
 def setup_argparse():
@@ -73,23 +118,8 @@ def setup_argparse():
         default=False,
     )
 
-    parser_tld = subparsers.add_parser(name="domains", help="View and configure domains.")
-    parser_tld.set_defaults(func=domains.main)
-
-    group_tld = parser_tld.add_mutually_exclusive_group(required=True)
-    group_tld.add_argument(
-        "-l",
-        "--list",
-        help="List domains registered in your DigitalOcean account, indicating which are managed.",
-        action="store_true",
-    )
-
-    group_tld.add_argument(
-        "-a",
-        "--add",
-        help="Add <domain> to domains managed by ddns-digital-ocean.",
-        metavar=("<domain>"),
-    )
+    configure_domains_subparser(subparsers)
+    configure_ip_lookup_subparser(subparsers)
 
     parser.add_argument(
         "-l",
@@ -115,15 +145,6 @@ def setup_argparse():
         help="List the current IP address for the sub-domain given",
         nargs=1,
         action="append",
-    )
-
-    parser.add_argument(
-        "-t",
-        "--top",
-        help=("Add new domain(s) from your DigitalOcean account to use as a dynamic DNS domain."),
-        required=False,
-        metavar=("domain"),
-        nargs="+",
     )
 
     parser.add_argument(
@@ -158,26 +179,6 @@ def setup_argparse():
         nargs=1,
         metavar=("test.example.com"),
         action="append",
-    )
-
-    parser_ip_server = subparsers.add_parser(
-        name="ip_lookup_config",
-        help=("Update the service/server used to lookup your public IP address."),
-    )
-    parser_ip_server.set_defaults(func=ip.view_or_update_ip_server)
-    parser_ip_server.add_argument(
-        "--url",
-        help=(
-            "The URL of the server to use for obtaining your current IP address. "
-            "NOTE: Expects the servers response to a GET request to have a .text response. "
-            "Example: https://api.ipify.org"
-        ),
-    )
-    parser_ip_server.add_argument(
-        "--ip-mode",
-        choices=["4", "6"],
-        default="4",
-        help=("IPv4 or IPv6. Which IP address to update. Default: %(default)s"),
     )
 
     parser_api_key = subparsers.add_parser(
