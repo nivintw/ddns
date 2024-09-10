8bcd2ce261c2e88eefcd5c4771a6f2a5d97c78c5
Tyler Nivin <tyler.nivin@gmail.com>
2024-09-10 00:15:55 -0500

    feature (config): Add support for XDG_DATA_HOME and default user-specific data dirs for mac and linux.
    Temporarily un-support windows.
    - Windows support added back once I figure out the Windows user-specific data dirs... this shouldn't be hard, I just never dev on windows.

--- src/ddns_digital_ocean/constants.py
+++ src/ddns_digital_ocean/constants.py
@@ -23,11 +23,34 @@
 # Copyright 2023 - 2024, Tyler Nivin <tyler@nivin.tech>
 #   and the ddns-digital-ocean contributors
 
+import os
+import platform
 from pathlib import Path
 
-homefilepath = Path.home()
-filepath = homefilepath.joinpath(".config/ddns")
-filepath.mkdir(parents=True, exist_ok=True)
 
-database_path = filepath.joinpath("ddns.db")
-logfile = filepath.joinpath("ddns.log")
+# Old Path...
+# /Users/tylernivin/.config/ddns...
+def set_app_data_home():
+    match platform.system():
+        case "Linux" | "Darwin":
+            # Linux or Macos.
+            # Technically... Macos wants us to use ~/Library/Application Support/...
+            # But, i'm not alone in saying... nah thanks.
+
+            # Use XDG_DATA_HOME if it's defined,
+            # else fall back to ~/.local/share per XDG spec.
+            # Ref: https://specifications.freedesktop.org/basedir-spec/latest/index.html
+            data_home = Path(os.environ.get("XDG_DATA_HOME", Path.home() / ".local" / "share"))
+        case "Windows":
+            # TODO: Phone a friend about Windows...
+            raise RuntimeError("Windows not yet supported.")
+        case _:
+            raise RuntimeError("Unknown platform; file a ticket to discuss support.")
+    return data_home
+
+
+app_data_home = set_app_data_home() / "tech.nivin.digital-ocean-dynamic-dns"
+app_data_home.mkdir(parents=True, exist_ok=True)
+
+database_path = app_data_home.joinpath("do_ddns.db")
+logfile = app_data_home.joinpath("do_ddns.log")
