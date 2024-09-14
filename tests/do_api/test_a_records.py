35e2d05bd62a843c4cb8290204457008724c67d0
Tyler Nivin <tyler.nivin@gmail.com>
2024-09-14 17:21:01 -0500

    tests (TestGetARecordsByName): Add TestGetARecordsByName.

--- tests/do_api/test_a_records.py
+++ tests/do_api/test_a_records.py
@@ -205,6 +205,220 @@ class TestGetARecords:
             next(do_api.get_A_records(EXPECTED_DOMAIN))
 
 
+class TestGetARecordsByName:
+    """We can retrieve all A records matching provided name for the provided domain.
+
+    Normally, we'd probably expect only one matching result, but technically this
+    request could return multiple matches as there is nothing stopping you from
+    creating multiple records with the same name.
+    """
+
+    # Constants common to get_A_record_by_name()
+    PAGE_RESULTS_LIMIT = 20
+
+    def test_single_page(
+        self,
+        mocked_responses: RequestsMock,
+        preload_api_key: Literal["sentinel-api-key"],
+    ):
+        """Simple A record retrieval validation."""
+        EXPECTED_DOMAIN = "test.domain.example.com"
+        EXPECTED_SUBDOMAIN = "@"
+        EXPECTED_DOMAIN_RECORDS = [
+            {"id": 1, "name": EXPECTED_SUBDOMAIN, "data": "127.0.0.1", "ttl": 1800},
+            {"id": 2, "name": EXPECTED_SUBDOMAIN, "data": "127.0.0.1", "ttl": 1800},
+        ]
+        headers = {
+            "Authorization": "Bearer " + preload_api_key,
+            "Content-Type": "application/json",
+        }
+        mocked_responses.get(
+            url=f"https://api.digitalocean.com/v2/domains/{EXPECTED_DOMAIN}/records",
+            match=[
+                matchers.header_matcher(headers),
+                matchers.query_param_matcher(
+                    {
+                        "name": f"{EXPECTED_SUBDOMAIN}.{EXPECTED_DOMAIN}",
+                        "per_page": self.PAGE_RESULTS_LIMIT,
+                        "type": "A",
+                    }
+                ),
+            ],
+            json={
+                "domain_records": EXPECTED_DOMAIN_RECORDS,
+                # Match observed DO api behavior; total returns TOTAL,
+                # not total for this response; despite what docs say...
+                # I reported to DO via a ticket.
+                "meta": {"total": len(EXPECTED_DOMAIN_RECORDS)},
+            },
+        )
+
+        domain_records = [
+            x for x in do_api.get_A_record_by_name(EXPECTED_SUBDOMAIN, EXPECTED_DOMAIN)
+        ]
+        assert domain_records == EXPECTED_DOMAIN_RECORDS
+
+    def test_no_matching_a_records(
+        self,
+        mocked_responses: RequestsMock,
+        preload_api_key: Literal["sentinel-api-key"],
+    ):
+        """When no A records are available, raise no errors; return empty generator."""
+        EXPECTED_DOMAIN = "test.domain.example.com"
+        EXPECTED_SUBDOMAIN = "@"
+        headers = {
+            "Authorization": "Bearer " + preload_api_key,
+            "Content-Type": "application/json",
+        }
+        mocked_responses.get(
+            url=f"https://api.digitalocean.com/v2/domains/{EXPECTED_DOMAIN}/records",
+            match=[
+                matchers.header_matcher(headers),
+                matchers.query_param_matcher(
+                    {
+                        "name": f"{EXPECTED_SUBDOMAIN}.{EXPECTED_DOMAIN}",
+                        "per_page": self.PAGE_RESULTS_LIMIT,
+                        "type": "A",
+                    }
+                ),
+            ],
+            json={"domain_records": [], "meta": {"total": 0}},
+        )
+
+        with pytest.raises(StopIteration):
+            next(do_api.get_A_record_by_name(EXPECTED_SUBDOMAIN, EXPECTED_DOMAIN))
+
+    def test_multiple_pages(
+        self,
+        mocked_responses: RequestsMock,
+        preload_api_key: Literal["sentinel-api-key"],
+    ):
+        """Pagination is supported."""
+        EXPECTED_DOMAIN = "test.domain.example.com"
+        EXPECTED_SUBDOMAIN = "@"
+        EXPECTED_DOMAIN_RECORDS = [
+            *[
+                x
+                for x in repeat(
+                    {"id": 1, "name": EXPECTED_SUBDOMAIN, "data": "127.0.0.1", "ttl": 1800},
+                    self.PAGE_RESULTS_LIMIT,
+                )
+            ],
+            {"id": 2, "name": EXPECTED_SUBDOMAIN, "data": "127.0.0.1", "ttl": 1800},
+        ]
+        headers = {
+            "Authorization": "Bearer " + preload_api_key,
+            "Content-Type": "application/json",
+        }
+        mocked_responses.get(
+            url=f"https://api.digitalocean.com/v2/domains/{EXPECTED_DOMAIN}/records",
+            match=[
+                matchers.header_matcher(headers),
+                matchers.query_param_matcher(
+                    {
+                        "name": f"{EXPECTED_SUBDOMAIN}.{EXPECTED_DOMAIN}",
+                        "per_page": self.PAGE_RESULTS_LIMIT,
+                        "type": "A",
+                    }
+                ),
+            ],
+            json={
+                "domain_records": EXPECTED_DOMAIN_RECORDS[: self.PAGE_RESULTS_LIMIT],
+                # Match observed DO api behavior; total returns TOTAL,
+                # not total for this response; despite what docs say...
+                # I reported to DO via a ticket.
+                "meta": {"total": len(EXPECTED_DOMAIN_RECORDS)},
+            },
+        )
+        mocked_responses.get(
+            url=f"https://api.digitalocean.com/v2/domains/{EXPECTED_DOMAIN}/records",
+            match=[
+                matchers.header_matcher(headers),
+                matchers.query_param_matcher(
+                    {
+                        "name": f"{EXPECTED_SUBDOMAIN}.{EXPECTED_DOMAIN}",
+                        "per_page": self.PAGE_RESULTS_LIMIT,
+                        "page": 2,
+                        "type": "A",
+                    }
+                ),
+            ],
+            json={
+                "domain_records": EXPECTED_DOMAIN_RECORDS[self.PAGE_RESULTS_LIMIT :],
+                # Match observed DO api behavior; total returns TOTAL,
+                # not total for this response; despite what docs say...
+                # I reported to DO via a ticket.
+                "meta": {"total": len(EXPECTED_DOMAIN_RECORDS)},
+            },
+        )
+
+        domain_records = [
+            x for x in do_api.get_A_record_by_name(EXPECTED_SUBDOMAIN, EXPECTED_DOMAIN)
+        ]
+        assert domain_records == EXPECTED_DOMAIN_RECORDS
+
+    @pytest.mark.parametrize(
+        "status_code, json_response",
+        [
+            pytest.param(
+                404,
+                {"id": "not_found", "message": "The resource you requested could not be found."},
+                id="invalid-top-domain",
+            ),
+            pytest.param(
+                401,
+                {"id": "unauthorized", "message": "Unable to authenticate you."},
+                id="unauthorized",
+            ),
+            pytest.param(
+                429,
+                {"id": "too_many_requests", "message": "API Rate limit exceeded."},
+                id="too_many_requests",
+            ),
+            pytest.param(
+                500,
+                {"id": "server_error", "message": "Unexpected server-side error"},
+                id="server_error",
+            ),
+        ],
+    )
+    def test_invalid_responses(
+        self,
+        mocked_responses: RequestsMock,
+        preload_api_key: Literal["sentinel-api-key"],
+        status_code: int,
+        json_response: dict[str, str],
+    ):
+        """Invalid responses are raised.
+        This includes if the domain specified can't be found.
+        """
+        EXPECTED_DOMAIN = "test.domain.example.com"
+        EXPECTED_SUBDOMAIN = "@"
+
+        headers = {
+            "Authorization": "Bearer " + preload_api_key,
+            "Content-Type": "application/json",
+        }
+        mocked_responses.get(
+            url=f"https://api.digitalocean.com/v2/domains/{EXPECTED_DOMAIN}/records",
+            match=[
+                matchers.header_matcher(headers),
+                matchers.query_param_matcher(
+                    {
+                        "name": f"{EXPECTED_SUBDOMAIN}.{EXPECTED_DOMAIN}",
+                        "per_page": self.PAGE_RESULTS_LIMIT,
+                        "type": "A",
+                    }
+                ),
+            ],
+            json=json_response,
+            status=status_code,
+        )
+
+        with pytest.raises(requests.exceptions.HTTPError, match=rf"{status_code}"):
+            next(do_api.get_A_record_by_name(EXPECTED_SUBDOMAIN, EXPECTED_DOMAIN))
+
+
 class TestCreateARecord:
     """We can create A records for given subdomain/domain pair."""
 
