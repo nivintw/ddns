e7d878e2b95f1f0a29f2bd3d91de908b99ce09ff
Tyler Nivin <tyler.nivin@gmail.com>
2024-09-15 22:40:34 -0500

    feat: :building_construction: Remove api key table
    Storing the API key in a table was redundant with reading it from an environment variable. The new approach means there's less to manage, is better for security (since we don't write the API token unencrypted into the database file), and simplifies management, maintenance, and understanding of usage.

--- src/digital_ocean_dynamic_dns/api_key_helpers.py
+++ src/digital_ocean_dynamic_dns/api_key_helpers.py
@@ -23,34 +23,13 @@
 # Copyright 2024 - 2024, Tyler Nivin <tyler@nivin.tech>
 #   and the digital-ocean-dynamic-dns contributors
 
-import datetime as dt
-import logging
 import os
-from argparse import Namespace
 
 from rich import print
-
-from . import constants
-from .database import connect_database
-
-conn = connect_database(constants.database_path)
 
 
 class NoAPIKeyError(Exception):
     """Raised when the user tries to do anything without first configuring an API key."""
-
-
-def view_or_update(args: Namespace):
-    """UX function that will update or show the API key value."""
-
-    api_key = args.api_key_value
-
-    if api_key is None:
-        curr_key = get_api()
-        print(f"Current API key is: {curr_key}")
-        return
-    else:
-        set_api_key(api_key)
 
 
 def get_api() -> str:
@@ -66,41 +45,13 @@ def get_api() -> str:
         2. The value stored in the database.
     """
     api_token = os.environ.get("DIGITALOCEAN_TOKEN")
-    if api_token:
-        return api_token
+    if api_token is None:
+        print(
+            "[red]Error:[/red] Missing APIkey. "
+            "Please set the DIGITALOCEAN_TOKEN environment variable!"
+        )
+        raise NoAPIKeyError(
+            "Missing API key. Please set the DIGITALOCEAN_TOKEN environment variable!"
+        )
 
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT key FROM apikey")
-    row = cursor.fetchone()
-
-    if row is None:
-        print("[red]Error:[/red] Missing APIkey. Please add one!")
-        raise NoAPIKeyError("Missing API key. Please add one!")
-
-    return row["key"]
-
-
-def set_api_key(api_value):
-    with conn:
-        now = dt.datetime.now()
-        update_datetime = now.strftime("%Y-%m-%d %H:%M")
-
-        cursor = conn.cursor()
-        cursor.execute("SELECT COUNT(*) FROM apikey")
-        count = cursor.fetchone()[0]
-
-        if count == 0:
-            cursor.execute(
-                "INSERT INTO apikey values(:id, :key, :last_updated)",
-                {"id": 1, "key": api_value, "last_updated": update_datetime},
-            )
-            logging.info(update_datetime + " - Info : API key added")
-            print("Your API key has been added.")
-        else:
-            cursor.execute(
-                "UPDATE apikey SET key = :key, last_updated = :last_updated WHERE id = 1",
-                {"key": api_value, "last_updated": update_datetime},
-            )
-            logging.info(update_datetime + " - Info : API key updated")
-            print("Your API key has been updated.")
+    return api_token
